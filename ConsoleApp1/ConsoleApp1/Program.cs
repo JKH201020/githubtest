bf02fafb7aa54c2fd859eb9173e678a710d44d72
jkh201020 <jack0517@naver.com>
2025-04-19 21:02:12 +0900

    fghj
    fghj

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Worlasdfasdfsdfadfd!");
+            Console.WriteLine("Hello, Worlafdghdfghsdfasdfsdfadfd!");
         }
     }
 }
